c1d4a9be0192c419758b93f821da785b0150745e
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-11 12:50:58 +0000

    [Finish] hachugram

--- app/controllers/reptiles_controller.rb
+++ app/controllers/reptiles_controller.rb
@@ -1,4 +1,5 @@
 class ReptilesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
     @reptiles = Reptile.all
   end
@@ -13,18 +14,27 @@ class ReptilesController < ApplicationController
   def create
     @reptile = Reptile.new(reptile_params)
     @reptile.user_id = current_user.id
-    @reptile.save
-    redirect_to reptile_path(@reptile)
+    if @reptile.save
+      redirect_to reptile_path(@reptile), notice: '投稿に成功しました。'
+    else
+      render 'new'
+    end
   end
 
   def edit
     @reptile = Reptile.find(params[:id])
+    if @reptile.user != current_user
+      redirect_to reptiles_path, alert: '不正なアクセスです。'
+    end
   end
 
   def update
     @reptile = Reptile.find(params[:id])
-    @reptile.update(reptile_params)
-    redirect_to reptile_path(@reptile)
+    if @reptile.update(reptile_params)
+      redirect_to reptile_path(@reptile), notice: '更新に成功しました。'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
