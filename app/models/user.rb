a3e6aa6fb25bf2f7b257a386eab11fde23188cbb
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-22 15:45:17 +0000

    [Add] フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true
   attachment :profile_image
+  
   has_many :reptiles, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
 
-  validates :username, presence: true
+# フォローのアソシエーション
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  def follow(user_id)
+   relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 
   def already_favorited?(reptile)
     self.favorites.exists?(reptile_id: reptile.id)
