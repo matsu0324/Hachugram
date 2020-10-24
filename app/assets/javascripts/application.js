28b2b654881bb9305e8cc2d0d7f086c65d653f0f
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-24 15:45:03 +0000

    [Add] いいね非同期

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,3 +41,18 @@ $(document).ready(function () {
     hidePrevious : false
   });
 });
+
+$(function () {
+  $(document).on('turbolinks:load', () => {
+    $('#user_postcode').jpostal({
+      postcode: [
+        '#user_postcode'
+      ],
+      address: {
+        "#user_prefecture_code": "%3",
+        "#user_address_city": "%4%5",
+        "#user_address_street": "%6%7"
+      }
+    });
+  });
+});
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,12 @@
 class FavoritesController < ApplicationController
   def create
+    @reptile = Reptile.find(params[:reptile_id])
     @favorite = current_user.favorites.create(reptile_id: params[:reptile_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @reptile = Reptile.find(params[:reptile_id])
     @favorite = current_user.favorites.find_by(reptile_id: @reptile.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
