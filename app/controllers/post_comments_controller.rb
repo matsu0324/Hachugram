2ae31223993970146a471327ad430959f4daa1b7
EC2 Default User <ec2-user@ip-172-31-22-206.ec2.internal>
2020-10-25 06:10:23 +0000

    [Add] コメント機能の非同期通信

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,14 +1,15 @@
 class PostCommentsController < ApplicationController
   def create
+    @reptile = Reptile.find(params[:reptile_id])
     reptile = Reptile.find(params[:reptile_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.reptile_id = reptile.id
     comment.save
-    redirect_to reptile_path(reptile)
   end
+
   def destroy
+    @reptile = Reptile.find(params[:reptile_id])
     PostComment.find_by(id: params[:id], reptile_id: params[:reptile_id]).destroy
-    redirect_to reptile_path(params[:reptile_id])
   end
 
   private
